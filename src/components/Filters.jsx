db9ec8cef06f367e1eb1ececfbf8c1de85b80ccf
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-02-15 14:40:59 -0500

    fix: hero

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -60,7 +60,7 @@ export const Filters = () => {
 
 
     return (
-        <div className='bg-filters w-[90%] md:w-[85%] m-auto text-white text-xs md:text-base rounded-2xl border-2 border-grayV1 absolute bottom-[-60px] left-0 right-0'>
+        <div className='bg-filters w-[90%] md:w-[85%] m-auto text-white text-xs md:text-base rounded-2xl border-2 border-grayV1 absolute bottom-[300px] left-0 right-0'>
             <div className='flex flex-col items-center lg:flex-row lg:justify-between gap-5 w-[95%] m-auto pt-8 pb-8'>
                 <div className='flex  md:gap-5 items-center'>
                     {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const Hero = () => {
     return (
-        <div className='bg-[url(/hero-image.jpg)] w-full h-screen bg-no-repeat bg-cover'>
-            <div className='w-[90%] lg:w-[80%] m-auto h-screen flex flex-col justify-center gap-4'>
+        <div className='bg-[url(/hero-image.jpg)] w-[100%] h-[50vh] bg-center
+ bg-no-repeat bg-cover'>
+            <div className='w-[90%] lg:w-[80%] m-auto h-[50vh] flex flex-col justify-center gap-4'>
                 <h1 className='text-5xl md:text-7xl font-bold md:w-[70%] lg:w-[50%] lg:leading-22'>Peace, nature, dream.</h1>
                 <p className='text-xl font-medium'>Find and book a great experience.</p>
             </div>
