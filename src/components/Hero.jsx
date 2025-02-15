4aee7ca7051807e771ccb2539bb19546d016afcb
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-02-15 15:10:04 -0500

    fix herov4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 export const Hero = () => {
     return (
-        <div className="w-full h-[80vh] flex flex-col items-center justify-center overflow-hidden">
-            <img src="/hero-image.jpg" className="w-full h-full object-cover" alt="Hero" />
-            <div className='w-[90%] absolute lg:w-[80%] m-auto h-[80vh] flex flex-col justify-center gap-4'>
+        <div className="bg-[url(/hero-image.jpg)] w-full h-[80vh] bg-no-repeat  bg-[length:100%_auto]">
+            <div className='w-[90%] lg:w-[80%] m-auto h-[80vh] flex flex-col justify-center gap-4'>
                 <h1 className='text-5xl md:text-7xl font-bold md:w-[70%] lg:w-[50%] lg:leading-22'>Peace, nature, dream.</h1>
                 <p className='text-xl font-medium'>Find and book a great experience.</p>
             </div>
