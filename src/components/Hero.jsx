52d96587a154d87849703ae43addfbdbb8c6e4b0
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-02-15 15:37:13 -0500

    fix: filter styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Hero = () => {
     return (
         <div >
-            <img src="/hero-image.jpg" className="w-[100%] h-[300px] md:h-[600px] relative object-bottom object-cover" alt="Hero" />
+            <img src="/hero-image.jpg" className="w-[100%] h-[400px] md:h-[600px] relative object-bottom object-cover" alt="Hero" />
             <div className='absolute top-[40px] md:top-[130px] left-[10px] md:left-[140px] w-[350px] md:w-[500px]'>
                 <h1 className='text-5xl md:text-7xl font-bold  lg:leading-22'>Peace, nature, dream.</h1>
                 <p className='text-xl font-medium'>Find and book a great experience.</p>
